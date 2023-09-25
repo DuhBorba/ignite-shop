4b7d9cd7caafa6ed5395861700b807830112538c
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-09-25 17:38:46 -0300

    Styles Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,29 @@
-import { styled } from '../styles'
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-})
+import tshirt1 from '../assets/tshirts/1.png'
+import tshirt2 from '../assets/tshirts/2.png'
+import tshirt3 from '../assets/tshirts/3.png'
 
 export default function Home() {
   return (
-    <Button>Enviar</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={tshirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 70,00</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={tshirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 70,00</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
