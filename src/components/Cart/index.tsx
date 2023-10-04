c41f44317cf46c8656850d4662d9422ff187dfe8
Eduardo Luis Borba <eduardoborba3@gmail.com>
2023-10-04 17:11:05 -0300

    Create Dialog Radix

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,7 +1,33 @@
 import { CartButton } from "../CartButton"
 
+import * as Dialog from '@radix-ui/react-dialog';
+import { CartClose, CartContent, CartProduct } from "./styles";
+import { X } from "@phosphor-icons/react";
+
 export const Cart = () => {
+  
   return (
-    <CartButton />
+    <Dialog.Root>    
+      <Dialog.Trigger asChild>
+        <CartButton />
+      </Dialog.Trigger>
+      
+      <Dialog.Portal>
+        <Dialog.Overlay />
+        <CartContent>
+          <CartClose>
+            <X size={24} weight="bold" />
+          </CartClose>
+          <Dialog.Title>
+            Sacola de compras
+          </Dialog.Title>
+          <section>
+            <CartProduct>
+              <p>Parece que seu carrinho está vario :(</p>
+            </CartProduct>
+          </section>
+        </CartContent>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
